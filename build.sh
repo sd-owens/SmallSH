d87dbc56e75241ba6e9a0490d532b19d85a8e77c
Steven Owens <owenss@oregonstate.edu>
2020-07-12 08:40:24 -0700

    added core program while loop

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
+echo "Compiling smallsh"
 gcc -std=c99 -g smallsh.c -o smallsh
+echo "Compiling cd"
+gcc -std=c99 -g cd.c -o cd
+echo "Compiling complete"
 ./smallsh
--- smallsh.h
+++ smallsh.h
@@ -1,8 +1,9 @@
-#define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -23,46 +24,48 @@ void print_array(int argc, char *argv[])
 int welcome()
 {
     fprintf(stdout, "Welcome to S2: Small Shell\n");
-    fprintf(stdout, ": ");
     fflush(stdout);
-   
-
 }
 
 int execute( char *argv[] )
 {
     int childStatus;
-    pid_t spawnPid = fork();
 
-    switch(spawnPid)
+    if(strcmp(argv[0], "cd") == 0)
     {
-        // fork() returns -1 if it fails to spawn a new child
-        case -1:
-            perror("fork() failed!");
-            exit(EXIT_FAILURE);
-            break;
-        // spawn id is 0 in the child process.   
-        case 0:
-            printf("Fork successful, CHILD(%d) running\n", getpid());
-            fflush(stdout);
+        //TODO add call to internal cd comment here.
+    }
+    else
+    {
+        pid_t spawnPid = fork();
 
-            execvp(argv[0], argv);
-            perror("execvp");
-            exit(EXIT_FAILURE);
-            break;
-        default:
+        switch(spawnPid)
+        {
+            // fork() returns -1 if it fails to spawn a new child
+            case -1:
+                perror("fork() failed!");
+                exit(EXIT_FAILURE);
+                break;
+            // spawn id is 0 in the child process.   
+            case 0:
+                printf("Fork successful, CHILD(%d) running\n", getpid());
+                fflush(stdout);
 
-            // if(strcmp(argv[argc - 1], "&") != 0)
-            //     spawnPid = waitpid(spawnPid, &childStatus, 0);
-            spawnPid = waitpid(spawnPid, &childStatus, 0);
+                execvp(argv[0], argv);
+                perror("execvp");
+                exit(EXIT_FAILURE);
+                break;
 
-            //TODO need function to wait for background processes to complete.
-            printf("PARENT(%d): child(%d) terminated.  Exiting\n", getpid(), spawnPid);
-            exit(EXIT_SUCCESS);
-            break;
+            default:
+
+                spawnPid = waitpid(spawnPid, &childStatus, 0);
+                //TODO need function to wait for background processes to complete.
+                //printf("PARENT(%d): child(%d) terminated.  Exiting\n", getpid(), spawnPid);
+                break;
+        }
     }
 
-
+    return(0);
 }
 
 int freeMem()
@@ -72,7 +75,9 @@ int freeMem()
 
 int parse(char *input, char *argv[])
 {
-
+        // look for &
+        // look for redirection
+        // look for $$ for expansion to process ID
 
 
 }
@@ -85,35 +90,42 @@ int run()
     int argc = 0, arg_size = 64;
     char* argv[512];
 
-    getline(&input, &nbytes, stdin);    // might need to change to newfd  
-
-    strtok(input, "\n");  // strip off newline char from end of user input.
-
-    argv[0] = (char *) malloc(sizeof(char) * arg_size);
-    assert(*argv != NULL);
-
-    argv[argc] = strtok(input, " ");  // break up user input into tokens seperate by spaces.
-
-    while (argv[argc] != NULL)
+    while(true)
     {
-        argc++;
-        argv[argc] = (char *) malloc(sizeof(char) * arg_size);
-        assert(argv[argc] != NULL);
-        argv[argc] = strtok(NULL, " ");
+        fprintf(stdout, ": ");
+        fflush(stdout);
 
+        getline(&input, &nbytes, stdin);    // might need to change to newfd  
+
+        strtok(input, "\n");  // strip off newline char from end of user input.
+
+        argv[0] = (char *) malloc(sizeof(char) * arg_size);
+        assert(*argv != NULL);
+
+        argv[argc] = strtok(input, " ");  // break up user input into tokens seperate by spaces.
+
+        while (argv[argc] != NULL)
+        {
+            argc++;
+            argv[argc] = (char *) malloc(sizeof(char) * arg_size);
+            assert(argv[argc] != NULL);
+            argv[argc] = strtok(NULL, " ");
+
+        }
+
+        // set last char* to NULL for passing to execvp
+        argv[argc] = NULL;  
+
+        //print_array(argc, argv);
+
+        execute(argv);
+        
+        //reset argc for next iteration
+        argc = 0;
     }
-
-    // set last char* to NULL for passing to execvp
-    argv[argc] = NULL;  
-
-    //print_array(argc, argv);
-
-    execute(argv);
-
+    
     free(input);
     
-
-
 }
 
 
